4294ccbd412cd928fba990259b0167bae2646691
Sunny-Gumber <68242840+Sunny-Gumber@users.noreply.github.com>
2026-08-29 16:04:45 +0530

    Keep V13.1 runtime contract for pronunciation enhancement

--- src/app.js
+++ src/app.js
@@ -68,7 +68,7 @@ async function boot(){
   if(isSignedIn()&&getState().prefs.dirty)syncNow('boot').catch(()=>{});
   registerServiceWorker();
 
-  window.LanguageLab={version:'13.2',getState:()=>structuredClone(getState()),user:()=>getUser(),sync:()=>syncNow('manual')};
+  window.LanguageLab={version:'13.1',getState:()=>structuredClone(getState()),user:()=>getUser(),sync:()=>syncNow('manual')};
 }
 
 boot().catch(error=>{

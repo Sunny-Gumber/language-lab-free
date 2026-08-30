47dfd58430577571adbfdb7c59b57232c7c62afd
Sunny-Gumber <68242840+Sunny-Gumber@users.noreply.github.com>
2026-08-30 09:07:49 +0530

    Update guest E2E for V14.0.1

--- e2e/guest.spec.js
+++ e2e/guest.spec.js
@@ -2,7 +2,7 @@ import{test,expect}from'@playwright/test';
 import{blockExternal}from'./helpers.js';
 
 async function waitForBoot(page){
-  await expect.poll(()=>page.evaluate(()=>window.LanguageLab?.version||0)).toBe('14.0');
+  await expect.poll(()=>page.evaluate(()=>window.LanguageLab?.version||0)).toBe('14.0.1');
   await expect(page.locator('.fatal-error')).toHaveCount(0);
 }
 async function openFirstRetrieve(page){
